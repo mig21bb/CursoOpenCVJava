c08008d6f70729a2f9fcc35305fd385de80da4ed
mig21bb <mig21bb@gmail.com>
2016-02-12 12:09:56 +0100

    All Functions added

--- Week2_02_LUTs/src/Week2_02_LUTs.java
+++ Week2_02_LUTs/src/Week2_02_LUTs.java
@@ -5,7 +5,7 @@ import org.opencv.core.Core;
 import static org.opencv.core.Core.LUT;
 import static org.opencv.core.CvType.CV_8U;
 import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class Week2_02_LUTs extends Application {
@@ -32,8 +32,11 @@ public class Week2_02_LUTs extends Application {
         System.out.println(lut.dump());
         
         for (int i = 0; i < 256; i++){
-		lut.put(0,i,255 - i); //Función Inversa
+//		lut.put(0,i,255 - i); //Función Inversa
+//              lut.put(0,i,Math.round(Math.sqrt(i*255)));// Función raíz cuadrada
+                lut.put(0,i,Math.round(Math.pow(i,3)/(255*255)));// Función cubica
 	}
+        
         System.out.println(lut.dump());
 	LUT(src, lut, result);
         NewStage s = new NewStage();
