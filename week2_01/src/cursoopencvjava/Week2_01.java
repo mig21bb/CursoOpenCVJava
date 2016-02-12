0bb7b93998b07ae1d1f3f394588d26d2651a8ff3
mig21bb <mig21bb@gmail.com>
2016-02-12 11:22:14 +0100

    week2_01 cleaned

--- week2_01/src/cursoopencvjava/Week2_01.java
+++ week2_01/src/cursoopencvjava/Week2_01.java
@@ -42,7 +42,7 @@ public class Week2_01 extends Application{
         
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         //Nombre del fichero / File name
-        String NombreImagen = "Tulips.jpg";
+        String NombreImagen = "ivvi.jpg";
         Mat src;
         //Cargamos la imagen y se comprueba que lo ha hecho correctamente
         //Get the image and check       
@@ -57,41 +57,33 @@ public class Week2_01 extends Application{
         List<Mat> bgr_planes = new ArrayList<>();
         Core.split(src, bgr_planes);
         
-        System.out.println(bgr_planes.get(0).toString());
-        System.out.println(bgr_planes.get(0).getClass());
-        
-        
         //Variables para el histograma
-	
-
 	/// los rangos (A,V,R) 
 	MatOfFloat range = new MatOfFloat(0,256);
 	MatOfInt histSize = new MatOfInt(255);
                
-        boolean uniform = true;
 	boolean accumulate = false;
         
         Mat b_hist = new Mat();
         Mat g_hist = new Mat();
-        Mat r_hist = new Mat();
-       
+        Mat r_hist = new Mat();       
         
         Imgproc.calcHist((List<Mat>) bgr_planes, new MatOfInt(0), new Mat(), b_hist, histSize, range, accumulate);
 	Imgproc.calcHist((List<Mat>) bgr_planes, new MatOfInt(1), new Mat(), g_hist, histSize, range, accumulate);
         Imgproc.calcHist((List<Mat>) bgr_planes, new MatOfInt(2), new Mat(), r_hist, histSize, range, accumulate);
-        System.out.println("Azul" + b_hist.toString()+b_hist.dump());
-        System.out.println("rojo" + r_hist.toString()+r_hist.dump());
-        System.out.println("Verde" + g_hist.toString()+g_hist.dump());
-        
                 
-        int hist_w = 512; int hist_h = 400;
+        // Dibujar el histograms para A, V y R        
+        int hist_w = 512;
+        int hist_h = 400;
         long bin_w;
         bin_w = Math.round((double)(hist_w /256));
+        
 	Mat histImage = new Mat(hist_h, hist_w, CvType.CV_8UC3,new Scalar(0,0,0));
+        /// Normalizar el resultado a [ 0, histImage.rows ]
         Core.normalize(b_hist,b_hist, 1, histImage.rows() , Core.NORM_MINMAX, -1, new Mat() ); 
         Core.normalize(g_hist,g_hist, 1, histImage.rows() , Core.NORM_MINMAX, -1, new Mat() ); 
         Core.normalize(r_hist,r_hist, 1, histImage.rows() , Core.NORM_MINMAX, -1, new Mat() ); 
-        
+        /// Dibujar para  cada canal
         for( int i = 0; i < 255; i++ ){
         line(
                 histImage,
@@ -113,91 +105,13 @@ public class Week2_01 extends Application{
                 2, 8, 0 );
                 }
                 
-            
+          //Mostrar imagen original e histograma a través de una nueva clase.
+          /// El método newWindow(Mat m) crea una ventana con la imagen m como parámetro.
             NewStage s = new NewStage();
             s.newWindow(src);
             s.newWindow(histImage);
             
-//        MatOfByte buffer = new MatOfByte();
-        
-        
-       
-//         encode the frame in the buffer
-//        int i = 0;
-//        for (Mat c:bgr_planes){
-//            
-//            Imgcodecs.imencode(".png", c, buffer);
-//            System.out.println("Canal cargado con imencode"+i);
-//            Image e = new Image(new ByteArrayInputStream(buffer.toArray()));
-//            images.add(e);
-//            System.out.println("imagen canal "+i +"  " +images.get(i).toString());
-//                        
-//            i += 1;
-//        }
-            
-        
-//         Create Image and ImageView objects
-            
-            
-            
-            
-            
-            
-
-//        Imgcodecs.imencode(".png", src, buffer);
-//        System.out.println("Cargada con imencode");
-//        Image src_im = new Image(new ByteArrayInputStream(buffer.toArray()));
-//        Imgcodecs.imencode(".png", histImage, buffer);
-//        System.out.println("Cargada con imencode");
-//        Image histogram = new Image(new ByteArrayInputStream(buffer.toArray()));
-//        System.out.println(histogram.toString());
-//         Create Image and ImageView objects
-//        
-//        
-//        
-//      
-//         Display image on screen
-//        Imgcodecs.imencode(".png", g_hist, buffer);
-//        Image image = new Image(new ByteArrayInputStream(buffer.toArray()));
-//        ImageView imageView0 = new ImageView();
-//        ImageView imageView1 = new ImageView();
-//        ImageView imageView2 = new ImageView();
-//        imageView0.setImage(src_im);
-//        imageView1.setImage(histogram);
-//        imageView2.setImage(images.get(2));
-//      
-//         Display image on screen
-//        StackPane im = new StackPane();
-//        StackPane his = new StackPane();
-//        his.getChildren().add(imageView0);
-//        his.getChildren().add(imageView1);
-//        root.getChildren().add(imageView1);
-//        root.getChildren().add(imageView2);
-//        Scene scene0 = new Scene(im);
-//        Scene scene1 = new Scene(his);
-//        primaryStage.setTitle("Image Read Test");
-//        primaryStage.setScene(scene0);
-//        primaryStage.setScene(scene1);
-//        primaryStage.show();
-
-    }
-//    public void newWindow(Mat m){
-//        Stage s = new Stage();
-//        MatOfByte buffer = new MatOfByte();
-//        Imgcodecs.imencode(".png", m, buffer);
-//        System.out.println(m.toString() + "Cargada con imencode en metodo");
-//        Image img = new Image(new ByteArrayInputStream(buffer.toArray()));
-//        StackPane pane = new StackPane();
-////        Text t = new Text(m.dump());
-//        ImageView imageView = new ImageView();
-//        imageView.setImage(img);
-//        pane.getChildren().addAll(imageView);
-//        Scene scene = new Scene(pane);
-//        s.setScene(scene);
-//        s.setTitle(m.toString());
-//        s.show();
-//        
-//    }
+   }    
  
     public static void main(String[] args) {
         launch(args);
